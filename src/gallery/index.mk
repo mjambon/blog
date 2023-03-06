2d2e96c08c3ef96606f984495f8ae224297c6d35
Martin Jambon <martin@mjambon.com>
2023-03-05 20:10:14 -0800

    Publish site

--- src/gallery/index.mk
+++
@@ -1,9 +0,0 @@
-GALLERY_PATHS = \
-  gallery/series/ser-20230111 \
-  gallery/series/ser-20230108 \
-  gallery/series/ser-20221030 \
-  gallery/series/ser-20221125 \
-  gallery/series/ser-20220305 \
-  gallery/series/ser-20220320 \
-  gallery/series/ser-20220212 \
-  gallery/series/ser-20220116
